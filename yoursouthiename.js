fdbe6d24a2da6db6be23b9a72b124e64e5d02f23
Michael Trachtman <mike@storyandstructure.com>
2013-07-19 16:29:42 -0400

    here's the response

--- yoursouthiename.js
+++ yoursouthiename.js
@@ -1,12 +1,19 @@
 if (Meteor.isClient) {
+  
+  Template.greeting.response = function() {
+    if (Session.get('ysn-name')) {
+      return 'tsup jonesie!';
+    }
+  };
 
   Template.form.events({
     'submit form' : function (e) {
       e.preventDefault();
       // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log($('#sas-name').val());
+      if (typeof console !== 'undefined') {
+        Session.set('ysn-name', $('#sas-name').val())
         $('#sas-name').val('').blur();
+      }
     }
   });
 }
